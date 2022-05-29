76c564e10b97b99ff1e220f52f9d68a926e6650f
JoshuaOdejide <joshua@bento.africa>
2022-05-29 19:22:59 +0100

    new updates

--- src/components/Museum.vue
+++ src/components/Museum.vue
@@ -1,7 +1,12 @@
 <template>
   <v-row>
     <v-col md="4" cols="12">
-      <v-card class="mx-auto tertiary1" max-width="400" elevation="6">
+      <v-card
+        height="410"
+        class="mx-auto tertiary1"
+        max-width="400"
+        elevation="6"
+      >
         <v-list-item two-line>
           <v-list-item-content>
             <v-list-item-title class="text-h5 tertiary2--text">
@@ -16,23 +21,38 @@
                 src="/images/akodi.png"
                 alt="Sunny image"
                 width="100%"
+                height="160"
               ></v-img>
             </v-col>
           </v-row>
         </v-card-text>
+
         <v-list-item class="tertiary2--text">
-          <p class="py-5 my-2 text-caption">
-            Akodi is an Educational platform whereby students get access to
-            learning materials and videos uploaded by their teachers/. It also
-            gives access to school owners to perform full Hr actives on the
-            teachers/ trainers
-          </p>
+          <div>
+            <p class="text-caption tertiary2--text">
+              Through this platform, students can access learning materials and
+              videos posted by their teachers. It also allows school owners to
+              perform various activities related to the teachers.
+            </p>
+            <v-btn
+              small
+              class="secondary--text mb-3"
+              target="_blank"
+              href="https://akodi-ui.vercel.app/"
+              >View</v-btn
+            >
+          </div>
         </v-list-item>
       </v-card>
     </v-col>
 
     <v-col md="4" cols="12">
-      <v-card class="mx-auto tertiary1" max-width="400" elevation="6">
+      <v-card
+        height="410"
+        class="mx-auto tertiary1"
+        max-width="400"
+        elevation="6"
+      >
         <v-list-item two-line>
           <v-list-item-content>
             <v-list-item-title class="text-h5 tertiary2--text">
@@ -48,18 +68,35 @@
                 src="/images/Screenshot.png"
                 alt="Sunny image"
                 width="100%"
+                height="180"
               ></v-img>
             </v-col>
           </v-row>
         </v-card-text>
         <v-list-item class="tertiary2--text">
-          <p class="py-5 my-2 text-caption">
-            Kadatemy is a platform whereby those interested in learning
-          </p>
+          <div>
+            <p class="text-caption tertiary2--text">
+              Kadatemy is a platform that enables people to learn various
+              tech-related courses at their own pace. It allows them to connect
+              with mentors and fellow students.
+            </p>
+            <v-btn
+              target="_blank"
+              href="https://www.kadatemy.com/"
+              small
+              class="secondary--text mb-3"
+              >View</v-btn
+            >
+          </div>
         </v-list-item>
       </v-card> </v-col
     ><v-col md="4" cols="12">
-      <v-card class="mx-auto tertiary1" max-width="400" elevation="6">
+      <v-card
+        height="410"
+        class="mx-auto tertiary1"
+        max-width="400"
+        elevation="6"
+      >
         <v-list-item two-line>
           <v-list-item-content>
             <v-list-item-title class="text-h5 tertiary2--text">
@@ -75,12 +112,27 @@
                 src="/images/ada.png"
                 alt="Sunny image"
                 width="100%"
+                height="180"
               ></v-img>
             </v-col>
           </v-row>
         </v-card-text>
-
-        <v-list-item> </v-list-item>
+        <v-list-item class="tertiary2--text">
+          <div>
+            <p class="text-caption">
+              Ada's Portfolio was created, displaying both her previous works
+              and projects, making it much easier to evaluate her for jobs,
+              roles, and positions.
+            </p>
+            <v-btn
+              target="_blank"
+              href="https://jovial-poitras-691ba0.netlify.app/"
+              small
+              class="secondary--text mb-3"
+              >View</v-btn
+            >
+          </div>
+        </v-list-item>
       </v-card>
     </v-col>
   </v-row>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -3,8 +3,7 @@
     <div class="text-h4 secondary--text">
       <h4>My works here 😁 .</h4>
     </div>
-
-    <v-img src="/images/code.svg" />
-    <Museum />
+    <v-img class="my-5" src="/images/code.svg" />
+    <Museum class="mt-7" />
   </v-container>
 </template>
