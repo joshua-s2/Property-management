82f26a7ea68e2a9461f4c8020a3a6b6e1ad766aa
JoshuaOdejide <joshua@bento.africa>
2024-11-13 16:44:40 +0100

    updated ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import { primary, secondary, tertiary1, tertiary2 } from "./vuetify.options";
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
